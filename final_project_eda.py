6e82461fd24b739bca60ebbe6063a40dc9733c29
zuksi <54374181+zuksi@users.noreply.github.com>
2021-06-16 12:10:11 +0300

    Update final_project_eda.py

--- final_project_eda.py
+++ final_project_eda.py
@@ -1,5 +1,5 @@
 # export SPARK_KAFKA_VERSION=0.10
-# /spark2.4/bin/pyspark --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,com.datastax.spark:spark-cassandra-connector_2.11:2.4.2 --driver-memory 512m --driver-cores 1 --master local[1]
+# /spark2.4/bin/pyspark --driver-memory 512m --driver-cores 1 --master local[1]
 
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
